d8b095fc7a012acc49b2413bbb0fc45fade5558e
saxenaakshit <akshit.saxena2003@gmail.com>
2023-03-20 23:18:32 +0530

    variables,objects added

---
+++ JavaScript/JS-I/index.js
@@ -0,0 +1,24 @@
+console.log("hello ji")
+
+
+//using variable
+let a=2;
+a='akshit';
+console.log(a);
+
+
+//using objects
+
+let person={
+    age:34,
+    names:'akshit'
+};
+console.log(person.age);
+
+
+
+
+
+
+
+
