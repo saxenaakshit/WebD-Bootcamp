4c07415e663b6d89daa4daf678f7118fcf413432
saxenaakshit <akshit.saxena2003@gmail.com>
2023-03-20 23:38:08 +0530

    some edits

--- JavaScript/JS-I/index.js
+++ JavaScript/JS-I/index.js
@@ -2,9 +2,22 @@ console.log("hello ji")
 
 
 //using variable
+//local
 let a=2;
 a='akshit';
 console.log(a);
+
+
+//global
+var c =28;
+var h='harshit';
+
+//convention
+
+//shouldn't start with numerics
+//should be in camelCase
+//should not contain space, -
+
 
 
 //using objects
@@ -14,6 +27,14 @@ let person={
     names:'akshit'
 };
 console.log(person.age);
+
+
+//using array
+//in the array of JS you can store multiple elements of multiple datatypes
+
+let namos=['rahul','aahil','babbar'];
+
+
 
 
 
