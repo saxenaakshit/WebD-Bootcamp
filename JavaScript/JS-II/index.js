407e04875fb9d8938826411f65f229ba31d3b101
saxenaakshit <akshit.saxena2003@gmail.com>
2023-03-28 00:17:10 +0530

    Js-II created  & object creation

---
+++ JavaScript/JS-II/index.js
@@ -0,0 +1,16 @@
+console.log("hello jee kese ho saare");
+
+
+//creating object
+//{}=object literal
+let rectangle ={
+
+length:4,
+breadth: 5,
+
+//creating function
+draw: function(){
+ console.log("function is called")
+}
+
+}
