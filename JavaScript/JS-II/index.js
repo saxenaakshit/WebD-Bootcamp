7e20bb495ee7c907139c425f653d7cd4c24a48af
saxenaakshit <akshit.saxena2003@gmail.com>
2023-03-29 11:30:15 +0530

    factory function added

--- JavaScript/JS-II/index.js
+++ JavaScript/JS-II/index.js
@@ -14,3 +14,44 @@ draw: function(){
 }
 
 }
+
+//1.factory function-->camelCaseNotation
+function createRectangle(){
+    let rectangle ={
+        length:4,
+        breadth: 5,
+        
+        //creating function
+        draw: function(){
+         console.log("function is called");
+        }
+        };
+}
+
+//storing the created object into a variable
+let rectangeObj1=createRectangle();
+ 
+
+//creating with custom values
+function createRectangle(length,breadth){
+    let rectangle ={
+        length:length, //or just length
+        breadth: breadth, //or just breadth
+        
+        //creating function
+        draw: function(){
+         console.log("function is called");
+        }
+        };
+}
+
+let rectangleObj2=createRectangle(5,4);
+
+
+
+//2.Constructor function-->PascalNotation
+ 
+
+
+
+
