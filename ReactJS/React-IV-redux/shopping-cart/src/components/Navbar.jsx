c62c6c8c80c841139fbb28ae55b1ecaf6bd27a9a
saxenaakshit <akshit.saxena2003@gmail.com>
2023-09-08 02:48:44 +0530

    shopping cart half done

--- ReactJS/React-IV-redux/shopping-cart/src/components/Navbar.jsx
+++ ReactJS/React-IV-redux/shopping-cart/src/components/Navbar.jsx
@@ -1,5 +1,29 @@
+import {FaShoppingCart} from "react-icons/fa"
+import { NavLink } from "react-router-dom";
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div>
+      <div className="flex flex-row justify-between bg-slate-500">
+       <NavLink to="/">
+       <img src="./logo2"/>
+       </NavLink>
+       
+        <div>
+          <NavLink>
+          <p>Home</p>
+          </NavLink>
+          <NavLink>
+          <div>
+         <FaShoppingCart/>
+         </div>
+          </NavLink>
+       
+          
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
--- ReactJS/React-IV-redux/shopping-cart/src/pages/Home.jsx
+++ ReactJS/React-IV-redux/shopping-cart/src/pages/Home.jsx
@@ -1,7 +1,44 @@
+import { useEffect, useState } from "react";
+import Spinner from "../components/Spinner";
+import Product from "../components/Product"
 const Home = () => {
   const API_URL = "https://fakestoreapi.com/products";
+  const [loading, setLoading] = useState(false);
+  const [posts, setPosts] = useState([]);
+  async function fetchProductData(){
+    setLoading(true);
+    try{
+      const res= await fetch(API_URL);
+      const data = await res.json();
+      setPosts(data);
+    }
+    catch(err){
+      console.log("its a error");
+      setPosts([]);
+    }
+    setLoading(false);
+  }
 
-  return <div>Home</div>;
+  useEffect( ()=> {
+    fetchProductData();
+  },[]);
+  return (
+    <div>
+      {
+        loading ? <Spinner/> :
+        posts.length >0 ? (
+          <div>{
+            posts.map( (post)=>(
+              <Product key={post.id} post={post}/>
+            ))}
+          </div>
+        ):
+        <div>
+          <p>No Data Found</p>
+        </div>
+      }
+    </div>
+  );
 };
 
 export default Home;
